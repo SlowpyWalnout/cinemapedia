0f4059f4513a564f94692b23092ec5fdc2d8b7ab
Slowpywalnout <jquintana2003@gmail.com>
2026-06-17 20:34:45 -0600

    datasource for getmoviebyid implemented

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -16,6 +17,27 @@ class MovieMapper {
     posterPath: (moviedb.posterPath != '')
         ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
         : 'no-poster',
+    releaseDate: moviedb.releaseDate,
+    title: moviedb.title,
+    video: moviedb.video,
+    voteAverage: moviedb.voteAverage,
+    voteCount: moviedb.voteCount,
+  );
+
+  static Movie movieDetailsToEntity(MovieDetails moviedb) => Movie(
+    adult: moviedb.adult,
+    backdropPath: moviedb.backdropPath != ''
+        ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+        : 'https://img.magnific.com/free-vector/hand-drawn-no-data-concept_52683-127823.jpg?semt=ais_hybrid&w=740&q=80',
+    genreIds: moviedb.genres.map((e) => e.name).toList(),
+    id: moviedb.id,
+    originalLanguage: moviedb.originalLanguage,
+    originalTitle: moviedb.originalTitle,
+    overview: moviedb.overview,
+    popularity: moviedb.popularity,
+    posterPath: (moviedb.posterPath != '')
+        ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+        : 'https://img.magnific.com/free-vector/hand-drawn-no-data-concept_52683-127823.jpg?semt=ais_hybrid&w=740&q=80',
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
     video: moviedb.video,
