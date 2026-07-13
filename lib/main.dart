1d6629ead00ff5bd7160d0bbc90a98b6975daedf
SlowpyWalnout <jquintana2003@gmail.com>
2026-07-13 06:58:17 -0600

    Test: insertion and deleting db tests

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,28 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:cinemapedia/config/database/database.dart';
+
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // await db
+  //     .into(db.favoriteMovies)
+  //     .insert(
+  //       FavoriteMoviesCompanion.insert(
+  //         movieId: 1,
+  //         backdropPath: 'backdropPath.png',
+  //         originalTitle: 'My first movie',
+  //         posterPath: 'posterPath.png',
+  //         title: 'mi primera pelicula',
+  //       ),
+  //     );
+  // final deleteQuery = db.delete(db.favoriteMovies);
+  // await deleteQuery.go();
+
+  // final movies = await db.select(db.favoriteMovies).get();
+  // print('movies: $movies');
+
   await dotenv.load(fileName: '.env');
   runApp(const ProviderScope(child: MainApp()));
 }
