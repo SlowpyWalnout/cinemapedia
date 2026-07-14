e0c3346c3b0ec77a6098287152db38af0eab56bd
SlowpyWalnout <jquintana2003@gmail.com>
2026-07-13 20:30:22 -0600

    Feat: Drift datasource implementation p.3

--- lib/infrastructure/datasources/drift_datasource.dart
+++ lib/infrastructure/datasources/drift_datasource.dart
@@ -21,9 +21,37 @@ class DriftDatasource extends LocalStorageDatasource {
   }
 
   @override
-  Future<List<Movie>> loadFavoriteMovies({int limit = 10, int offset = 0}) {
-    // TODO: implement loadFavoriteMovies
-    throw UnimplementedError();
+  Future<List<Movie>> loadFavoriteMovies({
+    int limit = 10,
+    int offset = 0,
+  }) async {
+    //query
+    final query = database.select(database.favoriteMovies)
+      ..limit(limit, offset: offset);
+    // ejecutar el query
+    final favoriteMoviesRows = await query.get();
+
+    final movies = favoriteMoviesRows
+        .map(
+          (row) => Movie(
+            adult: false,
+            backdropPath: row.backdropPath,
+            genreIds: const [],
+            id: row.movieId,
+            originalLanguage: '',
+            originalTitle: row.originalTitle,
+            overview: '',
+            popularity: 0,
+            posterPath: row.posterPath,
+            releaseDate: DateTime.now(),
+            title: row.title,
+            video: false,
+            voteAverage: row.voteAverage,
+            voteCount: 0,
+          ),
+        )
+        .toList();
+    return movies;
   }
 
   @override
