0368c246496cc4ceb6d6e10fc38dfc614508365a
SlowpyWalnout <jquintana2003@gmail.com>
2026-07-17 23:22:32 -0600

    Feat: Using favorite movies provider on favorites screen view.

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,15 +1,32 @@
+import 'package:cinemapedia/presentation/screens/storage/favorite_movies_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  ConsumerState<FavoritesView> createState() => _FavoritesViewState();
+}
+
+class _FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoriteMovies = ref.watch(favoriteMoviesProvider);
+    final myMovieList = favoriteMovies.values.toList();
+
     return Scaffold(
       body: ListView.builder(
-        itemCount: 10,
+        itemCount: favoriteMovies.keys.length,
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(title: Text('movie name'));
+          final movie = myMovieList[index];
+          return ListTile(title: Text(movie.title));
         },
       ),
     );
